e8a15bc428b3f1934cd5d2d34fb4c471c679cd2a
Gal Sesek <gal.sesek@endava.com>
2023-08-23 15:02:44 +0200

    Added challenge + updated Workbook 1.1

---
+++ Section_ONE/Challenge_1.java
@@ -0,0 +1,15 @@
+// Print your initials in console
+
+public class Challenge_1 {
+
+    public static void main(String[] args) {
+        System.out.println(" GGG    SSS");
+        System.out.println("G   G  S   S");
+        System.out.println("G      S    ");
+        System.out.println("G GGG   SSS ");
+        System.out.println("G   G      S");
+        System.out.println("G   G  S   S");
+        System.out.println(" GGG    SSS ");
+    }
+    
+}
--- Section_ONE/Workbook_1_1.java
+++ Section_ONE/Workbook_1_1.java
@@ -1,8 +1,8 @@
 // TASK 1: Make a class.
 
-//TASK 2: Inside the class, write the main() method.
+// TASK 2: Inside the class, write the main() method.
 
-//TASK 3: Print these messages to the console:
+// TASK 3: Print these messages to the console:
     // "Printing is fun!"
     //"Java > Python."
     //"I spilled my Java all over my paper."
