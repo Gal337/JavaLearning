bc73c42b70bb9cc52fc2982717272920a4b85de2
Gal Sesek <gal.sesek@endava.com>
2023-08-24 13:15:40 +0200

    Updated Variables.java file

--- Section_TWO/Variables.java
+++ Section_TWO/Variables.java
@@ -4,5 +4,8 @@ public class Variables {
     public static void main(String[] args) {
         int passengers = 5;
         System.out.println(passengers);
+
+        int busTicket = passengers;
+        System.out.println(busTicket);
     }
 }
