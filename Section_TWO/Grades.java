eb2abe0b1e8ff8a9298f4c243391837912fc2870
Gal Sesek <gal.sesek@endava.com>
2023-08-24 15:38:18 +0200

    Added Grades and Workbook_2_2.java with solution

---
+++ Section_TWO/Grades.java
@@ -0,0 +1,16 @@
+package Section_TWO;
+
+public class Grades {
+    public static void main(String[] args) {
+        
+        char potions = 'A';
+        char arithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println("Grade in Potions is " + potions);
+        System.out.println("Grade in Arithmancy is " + arithmancy);
+        System.out.println("Grade in Charms is " + charms);
+
+    }
+}
+
---
+++ Section_TWO/Workbook_2_2.java
@@ -0,0 +1,31 @@
+package Section_TWO;
+
+public class Workbook_2_2 {
+            public static void main(String[] args) {
+            // name (text)
+            String name = "Maddox";
+            // age (whole number)
+            int age = 36;
+            // country (text)
+            String country = "USA";
+            // sport (text)
+            String sport = "hockey";
+            // hours (whole number)
+            int hours = 2;
+            // game (text)
+            String game = "PUBG";
+            // subject (text)
+            String subject = "chemistry";
+            // grade (character)
+            char grade = 'A';
+         
+            System.out.println("My name is " +name+ ". I'm " +age+ " years old, and I'm from " +country+ ".");
+            System.out.println("My favourite sport is "+sport+". I play for " +hours+ " hours a day.");
+            System.out.println("When I'm tired, I like to play " +game+ ".");
+            System.out.println("In school, my favourite subject was " +subject+ ", I scored a " +grade+ ".");
+    
+            // Compare your result to what's on Learn the Part.
+        }
+    
+
+}
