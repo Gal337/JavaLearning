b79fae10f6f129afab4987a1ab8e5951dce3816f
Gal Sesek <gal.sesek@endava.com>
2023-08-31 09:20:22 +0200

    Added Logical operators file

--- Section_3/LogicalOperators.java
+++ Section_3/LogicalOperators.java
@@ -38,21 +38,30 @@ public class LogicalOperators {
         boolean hasGoodPerformance = true;
         boolean isLongTermEmployee = false;
 
-        // good performance or is a long term employee: System.out.println("The user is eligible for a promotion.");
-        // otherwise: System.out.println("The user is not eligible for a promotion.");
+        if (hasGoodPerformance || isLongTermEmployee) {
+            System.out.println("The user is eligible for a promotion.");
+        } else {
+            System.out.println("The user is not eligible for a promotion.");
+        }
 
         int userAge = 17;
         boolean isParentPresent = true;
 
-        // at least 18 or if parent is present: System.out.println("You can enter the venue.");
-        // otherwise: System.out.println("You cannot enter the venue.");
+        if (userAge >= 18 || isParentPresent) {
+            System.out.println("You can enter the venue.");
+        } else {
+            System.out.println("You cannot enter the venue.");
+        }
 
 
         int memberAge = 16;
         boolean hasMembership = false;
 
-        // at least 18 or has a membership: System.out.println("The user can access the service.");
-        // otherwise: System.out.println("The user cannot access the service.");
+        if (memberAge >= 18 || hasMembership) {
+            System.out.println("The user can access the service.");
+        } else {
+            System.out.println("The user cannot access the service.");
+        }
 
 
         /**********************NOT OPERATOR**********************/
@@ -60,22 +69,43 @@ public class LogicalOperators {
         String option = "cash";
 
         boolean cashOrCredit = option.equals("cash") || option.equals("credit");
+        boolean notCashOrCredit = !(option.equals("cash") || option.equals("credit"));
 
         // if payment option is NOT cash or credit: System.out.println("Please choose another payment option");
         // otherwise: System.out.println("Sold. Pleasure doing business with you!");
+        if (!(option.equals("cash") || option.equals("credit"))) {
+            System.out.println("Please choose another payment option.");
+        } else {
+            System.out.println("Sold. Pleasure doing business with you!");
+        }
+
 
         char letter = 'A';
 
         boolean isVowel = (letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U');
-
+        boolean notVowel = !((letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'));
+        
         // if letter is NOT a vowel: System.out.println("The letter " + letter + " is a consonant");
         // otherwise: System.out.println("The letter " + letter + " is a vowel");
+        if (!((letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U'))) {
+            System.out.println("The letter " + letter + " is a consonant.");
+        } else {
+            System.out.println("The letter " + letter + " is a vowel.");
+        }
+
 
         String move = "stay";
         boolean isHitOrStay = move.equals("hit") || move.equals("stay");
+        boolean notHitOrStay = !(move.equals("hit") || move.equals("stay"));
 
         // if move is NOT hit or stay: System.out.println("Please choose a valid move");
         // otherwise: System.out.println("You win 10 bucks!");
-
+        if (!(move.equals("hit") || move.equals("stay"))) {
+            System.out.println("Please choose a valid move");
+        } else {
+            System.out.println("You win 10 bucks!");
+        }
     }
+
 }
+
