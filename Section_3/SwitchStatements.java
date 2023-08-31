9385763573dbcc49764283e2653a12c288d97a8c
Gal Sesek <gal.sesek@endava.com>
2023-08-31 15:31:26 +0200

    Updated switch statements file

--- Section_3/SwitchStatements.java
+++ Section_3/SwitchStatements.java
@@ -3,6 +3,7 @@ public class SwitchStatements {
 
         // Section 1: Weather conditions
         String weather = "sunny";
+        
         switch (weather) {
             case "sunny": System.out.println("You can wear a shirt.");
                 break;
@@ -20,6 +21,7 @@ public class SwitchStatements {
 
         // Section 2: User role
         int role = 2;
+        
         switch (role) {
             case 1: System.out.println("You are an admin");
                 break;
@@ -54,21 +56,65 @@ public class SwitchStatements {
         // Section 5: Age and income
         int age = 25;
         int income = 50000;
+
+        if (age >= 18 && age <= 60 && income >= 30000) {
+            System.out.println("Congratulations! You are eligible for the credit card!\n");
+        } else {
+            System.out.println("Sorry, you are not eligible for the credit card.\n");
+        }
         // Determine eligibility for a specific credit card based on age and income
         // Decide whether to use if-else or switch
 
         // Section 1: Traffic light colors
         String lightColor = "green";
+
+        switch (lightColor) {
+            case "green": System.out.println("Go!");
+                break;
+            case "yellow": System.out.println("Slow down!");
+                break;
+            case "red": System.out.println("Stop!");
+                break;        
+            default: System.out.println("Power outage!");
+                break;
+        }
         // Determine what action to take based on the traffic light color (e.g., green, yellow, red)
         // Decide whether to use if-else or switch
 
         // Section 2: Exam grade
         int grade = 85;
+
+        if (grade >= 80) {
+            System.out.println("You got an A!");
+        } else if (grade >= 70) {
+            System.out.println("You got a B!");
+        } else if (grade >= 60) {
+            System.out.println("You got a C.");
+        } else if (grade >=50) {
+            System.out.println("You got a D.");
+        } else {
+            System.out.println("You failed.");
+        }
         // Determine the letter grade based on the numeric grade (e.g., A, B, C, D, F)
         // Decide whether to use if-else or switch
 
         // Section 3: Browser type
         String browser = "Chrome";
+
+        switch (browser) {
+            case "Chrome": System.out.println("Enjoy your Chrome extensions.");                
+                break;
+            case "Firefox": System.out.println("The best nonprofit browser.");                
+                break;
+            case "Safari": System.out.println("You are using Apple?");                
+                break;
+            case "Edge": System.out.println("There are better browsers.");                
+                break;
+            case "Opera": System.out.println("This browser still exists?");                
+                break;
+            default: System.out.println("Curious what you are using.");
+                break;
+        }
         // Check if the browser is one of the following: Chrome, Firefox, Safari, Edge, or Opera
         // Decide whether to use if-else or switch
         
