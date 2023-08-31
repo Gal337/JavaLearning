c222f52f7c11683777c17cd0381295dcd415a484
Gal Sesek <gal.sesek@endava.com>
2023-08-31 15:35:04 +0200

    Added Workbook 3.6

---
+++ Section_3/Workbook_3_6.java
@@ -0,0 +1,22 @@
+// TASK 1:
+/*
+ Check that day against 7 days from your calendar.
+case Monday, print: Sorry, I have to stay at work late.
+case Tuesday, print: It looks like I have meetings all day.
+case Wednesday, print: I have a dentist appointment. Some other time!
+case Thursday, print: Sorry, thursday is date night!
+case Friday, print: I'm free!!
+case Saturday, print: I'm free!!
+case Sunday, print: I'm free!!
+default: That's not a day.
+ */
+
+public class Workbook_3_6 {
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+    
+}
