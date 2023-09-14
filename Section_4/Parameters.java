072018d81551dabfc5cff4b768c3aefa171c89eb
Gal Sesek <gal.sesek@endava.com>
2023-09-14 11:21:02 +0200

    Added Workbook 4.2 and updated Parameters file

--- Section_4/Parameters.java
+++ Section_4/Parameters.java
@@ -3,13 +3,13 @@ public class Parameters {
     public static void main(String[] args) {
         
         // Values in () are called arguments, which are then passed to function
-        calculateArea(2.3, 3.5);
+        calculateArea(2.3, 3.6);
         calculateArea(1.6, 2.4);
         calculateArea(2.6, 4.3);
 
     }
 
-    // double length = first argument, double width = second argument from () inside main function
+    // double length = first parameter which recieves first argument, double width = second parameter which recieves second argument from inside main function
     public static void calculateArea(double length, double width) {
 
             double area = length * width;
---
+++ Section_4/Workbook_4_2.java
@@ -0,0 +1,42 @@
+// Tip the waiter.
+// Your bill was $53.50. It's polite to tip the waiter 15% of your bill.
+
+// TASK 1: You will write a function that does this task. Function name: tipTheWaiter. It calculates a tip and prints it.
+/*
+Parameters: The function takes one parameter named bill of type double.
+Return value: the comment says nothing about a return value. So, you can assume the function is void.
+Inside the function, implement the following logic:
+
+ *  Function name: tipTheWaiter - calculates a tip and prints it.
+ *  @param bill (double).
+ *
+ *  Inside the function:   <-----
+ *   1. Calculates a tip that's 15 percent of the bill you paid.
+ *   2. Prints: your service was wonderful! Please, accept this tip: $<tip>
+
+
+ */
+
+ // TASK 2: Call the function from main().
+
+public class Workbook_4_2 {
+
+    public static void main(String[] args) {
+        
+        double bill = 53.50;
+
+        System.out.println("Waiter: I hope you enjoyed your meal!");
+        
+        tipTheWaiter(53.50);
+
+    }
+
+    public static void tipTheWaiter (double bill) {
+
+        double tip = bill*0.15;
+        
+        System.out.println("\nYour service was wonderful! Please, accept this tip: " + tip);
+
+    }
+
+}
