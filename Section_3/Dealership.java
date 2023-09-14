abaf3e301439fc1118305e1d775bcc35bc78ce17
Gal Sesek <gal.sesek@endava.com>
2023-09-14 10:22:16 +0200

    Updated Dealership file and Parameters file

--- Section_3/Dealership.java
+++ Section_3/Dealership.java
@@ -51,6 +51,7 @@ public class Dealership {
                 break;
         }
 
+        scan.close();
 
         }
 }
--- Section_4/Parameters.java
+++ Section_4/Parameters.java
@@ -2,8 +2,18 @@ public class Parameters {
 
     public static void main(String[] args) {
         
-        
+        // Values in () are called arguments, which are then passed to function
+        calculateArea(2.3, 3.5);
+        calculateArea(1.6, 2.4);
+        calculateArea(2.6, 4.3);
 
     }
+
+    // double length = first argument, double width = second argument from () inside main function
+    public static void calculateArea(double length, double width) {
+
+            System.out.println("Area: " + length * width);
+
+        }
     
 }
