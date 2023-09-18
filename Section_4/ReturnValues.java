75bda8890e35b6f6decc4629cbe038cb2383b198
Gal Sesek <gal.sesek@endava.com>
2023-09-18 14:07:31 +0200

    Updated ReturnValues file

--- Section_4/ReturnValues.java
+++ Section_4/ReturnValues.java
@@ -3,9 +3,11 @@ public class ReturnValues {
     public static void main(String[] args) {
         
         // Values in () are called arguments, which are then passed to function
-        calculateArea(2.3, 3.6);
-        calculateArea(1.6, 2.4);
-        calculateArea(2.6, 4.3);
+        double area1 = calculateArea(2.3, 3.6);
+        double area2 = calculateArea(1.6, 2.4);
+        double area3 = calculateArea(2.6, 4.3);
+
+        System.out.println("Area 1: " + area1 + "\nArea 2: " + area2 + "\nArea 3: " + area3);
 
     }
 
