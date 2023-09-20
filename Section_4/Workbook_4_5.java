29465abbf6b1bbc1b50ee8c05d0bb1b159b2d24a
Gal Sesek <gal.sesek@endava.com>
2023-09-20 12:39:23 +0200

    Prepared Workbook 4.5 file

---
+++ Section_4/Workbook_4_5.java
@@ -0,0 +1,86 @@
+/**
+ * TASK 1: Calculate area of a square
+ * Name: areaSquare  <------
+ * @param side (double)
+ * @return the area (double)
+ *
+ * Inside the function:
+ *  1. If side is negative
+ *        - prints "Impossible"
+ *        - shuts the app down
+ *  2. Calculates the area of the square. A = side²
+ */
+
+ /**
+ * TASK 2: Calculate area of a rectangle
+ * Name: areaRectangle    <------
+ * @param length (double).
+ * @param width  (double).
+ * @return the area (double)
+ *
+ * Inside the function:
+ *  1. If the length OR width is negative
+ *       - print "Error: impossible" and
+ *       - terminate the program.
+ *  2. return the area: length * width
+ */
+
+ /**
+ * TASK 3: Calculate area of a triangle
+ * Name: areaTriangle
+ * @param base: (double).
+ * @param height: (double).
+ * @return the area (double)
+ *
+ * Inside the function:
+ *  1. If the base OR height is negative
+ *      - print "Error: impossible"
+ *      - terminate the program.
+ *  2. return the area: base * height / 2
+ */
+
+ /**
+ * TASK 4: Calculate area of a circle
+ * Name: areaCircle
+ * @param radius (double).
+ * @return area (double)
+ *
+ * Inside the function:
+ *  1. If the radius is negative
+ *      - print: Impossible
+ *      - terminates the program
+ *  2. returns the area: π * radius2
+ */
+
+ /**
+ * TASK 5: 
+ * Using the appropriate function, calculate the area for:
+ * a square with a side of 2 cm.
+ * a rectangle with a length of 1 cm, and a width of 2 cm.
+ * a triangle with a base of 1 cm, and a height of 2 cm.
+ * a circle with a radius of 2 cm
+ */
+
+ /**
+ * TASK 6: Write a function that prints every area
+ * Name: printAreas -- it prints four areas
+ * @param square (double)
+ * @param rectangle (double)
+ * @param triangle (double)
+ * @param circle (double)
+ *
+ * Inside the function:
+ *  1. print: ("Square area: <square area>")
+ *  2. print: ("Rectangle area: <rectangle area>")
+ *  3. print: ("Triangle area: <triangle area>")
+ *  4. print: ("Circle area: <circle area>")
+ */
+
+ /**
+  * TASK 7: Call the printAreas function
+  */
+
+
+public class Workbook_4_5 {
+    
+}
