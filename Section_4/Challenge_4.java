830779a863b1f976af011cc83ba692261e35686c
Gal Sesek <gal.sesek@endava.com>
2023-09-21 12:57:23 +0200

    Added instructions to Challenge 4 file

--- Section_4/Challenge_4.java
+++ Section_4/Challenge_4.java
@@ -1,3 +1,118 @@
+/**
+ * Task 1: See if the user wants to play
+
+Use Scanner to pick up the user's response.
+>>Let's play Rock Paper Scissors.
+>>When I say 'shoot', Choose: rock, paper, or scissors.
+>>
+>>Are you ready? Write 'yes' if you are
+ */
+
+ /**
+  * Task 2: Set up the game.
+
+- if the answer is yes:
+   -- print: Great!
+   -- print: rock - paper - scissors, shoot!
+   -- pick up user's choice.
+   -- get the computer choice (can be done after task 3).
+   -- get the result (can only be done after task 4)
+   -- print everything (can only be done after task 5).
+- else:
+   -- print: Darn, some other time...!
+
+Here, the user would enter rock paper or scissors. Using Scanner, pick up their choice.
+- if the answer is yes:
+   -- print: Great!
+   -- print: rock - paper - scissors, shoot!
+   -- pick up user's choice. <----------
+   -- get the computer choice (can be done after task 3).
+   -- get the result (can only be done after task 4)
+   -- print everything (can only be done after task 5).
+- else:
+   -- print: Darn, some other time...
+
+There are more instructions for the if statement. You cannot do them until you finish tasks 3, 4, and 5.
+- if the answer is yes:
+   -- print: Great!
+   -- print: rock -- paper -- scissors, shoot!
+   -- pick up user's choice.
+   -- get the computer choice (can be done after task 3). <------
+   -- get the result (can only be done after task 4) <-------
+   -- print everything (can only be done after task 5). <--------
+- else:
+   -- print: Darn, some other time...
+
+So, you're left with the else block. If the user doesn't enter "yes", print: Darn, some other time...!
+- if the answer is yes:
+   -- print: Great!
+   -- print: rock -- paper -- scissors, shoot!
+   -- pick up user's choice.
+   -- get the computer choice (can be done after task 3).
+   -- get the result (can only be done after task 4)
+   -- print everything (can only be done after task 5).
+- else: <---------------
+   -- print: Darn, some other time...
+  */
+
+  /**
+   * Task 3: Generate a random choice
+
+
+
+You'll write a function that picks randomly between rock paper scissors.
+Function name:
+
+
+ * Function name: computerChoice <--------
+ * @return a choice (String).
+ *
+ * Inside the function:
+ *   1. Picks a random number between 0 and 2.
+ *   2. if 1: return the choice 'rock'
+ *      if 2: return the choice 'paper'
+ *      if 3: return the choice 'scissors'
+
+ Parameters: The function takes no parameters.
+ Return type:
+
+ * Function name: computerChoice
+ * @return a choice (String). <------
+ *
+ * Inside the function:
+ *   1. Picks a random number between 0 and 2.
+ *   2. if 0: return the choice 'rock'
+ *      if 1: return the choice 'paper'
+ *      if 2: return the choice 'scissors'
+ 
+ Inside the function, generate a random number between 0 and 2. Based on the result, return rock, paper or scissors.
+
+ * Function name: computerChoice
+ * @return a choice (String).
+ *
+ * Inside the function: <----------
+ *   1. Picks a random number between 0 and 2.
+ *   2. if 0: return the choice 'rock'
+ *      if 1: return the choice 'paper'
+ *      if 2: return the choice 'scissors'
+
+Call the function and store the computer choice.
+- if the answer is yes:
+   -- print: Great!
+   -- print: rock -- paper -- scissors, shoot!
+   -- pick up user's choice.
+   -- get the computer choice (can be done after task 3). <------------
+   -- get the result (can only be done after task 4)
+   -- print everything (can only be done after task 5).
+- else:
+   -- print: Darn, some other time...
+   */
 public class Challenge_4 {
     
+    public static void main(String[] args) {
+        
+
+
+    }
+
 }
