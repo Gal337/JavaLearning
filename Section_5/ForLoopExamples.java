3a50afc1ad814a929dfe1b8f5e22159c588a339f
Gal Sesek <gal.sesek@endava.com>
2023-09-25 14:05:49 +0200

    Updated ForLoopExamples file

--- Section_5/ForLoopExamples.java
+++ Section_5/ForLoopExamples.java
@@ -3,12 +3,14 @@ public class ForLoopExamples {
     public static void main(String[] args) {
         // Example 1: Print numbers from 1 to 10
         // TODO: Write a for loop that prints numbers from 1 to 10
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         // Example 2: Print "Hello, world!" 5 times
         // TODO: Write a for loop that prints "Hello, world!" 5 times
-
+        for (int i = 0; i <= 4; i++) {
+            System.out.println("Hello, world!");
+        }
         // Example 3: Print the multiplication table of 5 up to 10
         // TODO: Write a for loop that prints the multiplication table of 5 up to 10 (5 x 1, 5 x 2, ..., 5 x 10)
 
