192fb9d19da14349e7fa9944931cd0d74786293e
Gal Sesek <gal.sesek@endava.com>
2023-09-25 15:18:39 +0200

    Added print vs println example file

---
+++ Section_5/Print_vs_Println.java
@@ -0,0 +1,22 @@
+public class Print_vs_Println {
+    
+    public static void main(String[] args) {
+        
+        // println() prints text and moves to a new line
+        // Output:
+        // a
+        // b
+        // c
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+
+        // print() prints text but it does not move to a new line
+        // Output: a b c
+        System.out.print("a");
+        System.out.print("b");
+        System.out.print("c");
+
+    }
+
+}
