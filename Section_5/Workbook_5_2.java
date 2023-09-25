89bb1e8543fa59c21dee21fd668899d7168577c4
Gal Sesek <gal.sesek@endava.com>
2023-09-25 15:14:00 +0200

    Added Workbook 5.2 with solution

--- Section_5/Workbook_5_2.java
+++ Section_5/Workbook_5_2.java
@@ -1,3 +1,24 @@
+/* Task 1: Ask Bart to enter the line.
+Ask Bart to enter the line he wants to write.
+I will not forget semi-colons;
+*/
+
+import java.util.Scanner;
+
 public class Workbook_5_2 {
     
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hi Bart. I can write lines for you.");
+        System.out.println("What do you want me to write?");
+        
+        String input = scan.nextLine();
+
+        for (int i = 1; i < 100; i++) {
+            System.out.println(i + ". " + input);
+        }
+
+    }
+
 }
