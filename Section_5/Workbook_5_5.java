b76eb7bc784da8da3f472ca8b0112618caf07d42
Gal Sesek <gal.sesek@endava.com>
2023-09-27 10:07:19 +0200

    Updated Workbook 5.5 with solution

--- Section_5/Workbook_5_5.java
+++ Section_5/Workbook_5_5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Workbook 5.5 considers user input to define the start, stop, and step.
  * I hear you like to count by threes.
@@ -31,9 +33,33 @@
 public class Workbook_5_5 {
     
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);  
+
+        System.out.println("I hear you like to count by threes.");
+        System.out.println("Jimmy: It depends.");
+        System.out.println("Oh, ok...");
         
-        
+        System.out.print("1. Pick a number to count by: ");
+        int countBy = scan.nextInt();
+
+        System.out.print("2. Pick a number to start counting from: ");
+        int startCount = scan.nextInt();
+
+        System.out.print("3. Pick a number to count to: ");
+        int countTo = scan.nextInt();
+
+        for (int i = startCount ; i <= countTo; i += countBy) {
+          System.out.print(i + " ");
+        }
 
     }
 
 }
+
+
+/* Task 2 - Write a loop
+   Make a for loop that:
+   starts from the second number.
+   stops at the third number.
+   counts in steps of the first.
+   Then, print your count in ONE line. */
